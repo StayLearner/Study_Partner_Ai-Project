35a2721f69e3ce1f0ac826aef7d204d0ddefdddb
Rohan Ghosh <rohanghosh078@gmail.com>
2025-02-08 22:36:33 +0530

    create new function genrateNotes in inngest -> function.js and adding a new response PROMPT in ai model for this using Ai

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,7 +1,8 @@
 import { db } from "@/configs/db";
 import { inngest } from "./client";
-import { USER_TABLE } from "@/configs/schema";
+import { CHAPTER_NOTES_TABLE, USER_TABLE } from "@/configs/schema";
 import { eq } from "drizzle-orm";
+import { generateNotesAiModel } from "@/configs/AiModel";
 
 /* This code snippet is exporting a constant named `helloWorld` which is assigned the result of calling
 the `inngest.createFunction` function. This function is creating a function with the ID
@@ -71,3 +72,51 @@ export const CreateNewUser = inngest.createFunction(
 
   //send email notification after 3 days
 )
+
+
+
+
+
+
+
+
+/* The code snippet `export const GenerateNotes =
+inngest.createFunction({id:'generate-course'},{event:'notes.generate'}, async({event,step})=>{ const
+{course}= event.data; })` is defining a constant named `GenerateNotes` that is assigned the result
+of calling the `inngest.createFunction` function. This function is creating a function with the ID
+"generate-course" that listens for the event "notes.generate". */
+
+export const GenerateNotes=inngest.createFunction(
+  {id:'generate-course'},
+  {event:'notes.generate'},
+  async({event,step})=>{
+   const {course}= event.data;
+
+   const notesResult=await step.run('Generate Chapter Notes',async()=> {
+    const Chapters=course?.courselayout;
+    let index=0;
+    Chapters.forEach(async(chapter) => {
+    const PROMPT='Generate exam material detail content for each chapter , Make sure to includes all topic point in the content, make sure to give content in HTML format (Do not Add HTML , Head, Body, title tag), The chapters :' +JSON.stringify(chapter);  
+     
+    const result= await generateNotesAiModel.sendMessage(PROMPT);
+    const aiResp= result.response.text();
+
+    await db.insert(CHAPTER_NOTES_TABLE).values({
+      chapterId:index,
+      courseId:course?.courseId,
+      notes:aiResp
+    })
+
+    index=index+1;
+
+  });
+
+    return 'Completed'
+   })
+  }
+)
+
+
+
+
+
