8cddfdb70d38b5c795771386fa91649e35fd7c4b
Rohan Ghosh <rohanghosh078@gmail.com>
2025-02-09 11:08:11 +0530

    fetching data in courseId->page.jsx for showing in frontend

--- app/course/[courseId]/page.jsx
+++ app/course/[courseId]/page.jsx
@@ -1,15 +1,38 @@
 "use client"
 
 import DashboardHeader from '@/app/dashboard/_components/DashboardHeader';
+import axios from 'axios';
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Course() {
 
     const {courseId}= useParams();
-  return (
+    const [course,setCourse]= useState();
+    useEffect(()=>{
+        GetCourse();
+    },[])
+
+     const GetCourse=async () => {
+         const result=await axios.get('/api/courses?courseId='+courseId)
+         console.log(result);
+         setCourse(result.data.result); 
+     }
+  
+    return (
     <div>
         <DashboardHeader/>
+
+        // Course Intro
+
+
+        //Study Materials Options
+
+
+
+        // Chapter List
+
+        
     </div>
   )
 }
